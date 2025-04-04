3047b5e5fd2ba5fab8804d934259491954ab9d92
C. Spencer Beggs <spencer@beg.gs>
2025-04-04 16:19:43 -0400

    build: setup project configurations and tooling
    Add configurations for commit linting, prettier formatting, and ESLint.
    Include .gitignore for node_modules.
    Establish basic TypeScript configuration.
    Set up Husky for git hooks and prettier for consistent code styling.
    Add pnpm configuration for workspace and package management.
    Create initial package.json with scripts and dependencies.
    
    Facilitates better code quality assurance and project management through tooling.

---
+++ eslint.config.ts
@@ -0,0 +1,67 @@
+import eslint from "@eslint/js";
+import { createTypeScriptImportResolver } from "eslint-import-resolver-typescript";
+import importPlugin from "eslint-plugin-import-x";
+import packageJsonPlugin from "eslint-plugin-package-json";
+import prettierPlugin from "eslint-plugin-prettier/recommended";
+import tseslint from "typescript-eslint";
+import type { FlatConfig } from "@typescript-eslint/utils/ts-eslint";
+
+const config = tseslint.config(
+	eslint.configs.recommended,
+	tseslint.configs.strictTypeChecked,
+	tseslint.configs.stylisticTypeChecked,
+	importPlugin.flatConfigs.recommended,
+	importPlugin.flatConfigs.typescript,
+	{
+		name: "typescript",
+		files: ["**/*.ts", "**/*.tsx", "**/*.mts", "**/*.cts"],
+		ignores: ["package.json"],
+		languageOptions: {
+			ecmaVersion: "latest",
+			parserOptions: {
+				projectService: true,
+				allowDefaultProject: true,
+				tsconfigRootDir: import.meta.dirname
+			}
+		},
+		settings: {
+			"import-x/resolver-next": [
+				createTypeScriptImportResolver({
+					alwaysTryTypes: true,
+					project: ["tsconfig.json"]
+				})
+			]
+		},
+		rules: {
+			"@typescript-eslint/consistent-type-imports": [
+				"error",
+				{
+					prefer: "type-imports",
+					fixStyle: "separate-type-imports"
+				}
+			],
+			"import-x/order": [
+				"error",
+				{
+					groups: ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"],
+					"newlines-between": "never",
+					alphabetize: {
+						order: "asc",
+						caseInsensitive: true
+					}
+				}
+			],
+			"import-x/no-named-as-default-member": ["off"]
+		}
+	},
+	packageJsonPlugin.configs.recommended,
+	{
+		files: ["**/package.json"],
+		extends: [tseslint.configs.disableTypeChecked]
+	},
+	prettierPlugin
+);
+
+export default config.filter(
+	(item, index, self) => index === self.findIndex((t) => t.name === item.name)
+) satisfies FlatConfig.ConfigArray;
