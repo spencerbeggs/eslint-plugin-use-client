1fabd1a7d3e1ae404d1012dd031417f89aa80bc0
C. Spencer Beggs <spencer@beg.gs>
2025-04-06 23:21:46 -0400

    build(pkg): add package.json processing in build step
    Adds functionality to read and process package.json during the build process.
    
    Removes devDependencies and scripts before writing the processed file to the distribution folder.
    
    Updates the exports to point to the dist files for better compatibility.
    
    Also, includes the addition of the `type-fest` dependency to enhance type definitions.

--- pkg/tsup.config.ts
+++ pkg/tsup.config.ts
@@ -1,6 +1,7 @@
-import { copyFile } from "fs/promises";
+import { copyFile, readFile, writeFile } from "fs/promises";
 import { join } from "path";
 import { defineConfig } from "tsup";
+import type { PackageJson } from "type-fest";
 
 export default defineConfig({
 	entry: ["src/index.ts"],
@@ -18,6 +19,27 @@ export default defineConfig({
 	tsconfig: "tsconfig.json",
 	async onSuccess() {
 		await copyFile(join(process.cwd(), "..", "LICENSE"), join(process.cwd(), "dist", "LICENSE"));
-		console.log("Successfully built plugin");
+
+		// Read and process package.json
+		const packageJsonPath = join(process.cwd(), "package.json");
+		const packageJson: PackageJson = JSON.parse(await readFile(packageJsonPath, "utf-8")) as PackageJson;
+
+		// Remove devDependencies and scripts
+		const { devDependencies, scripts, ...rest } = packageJson;
+
+		// Update exports to point to dist files
+		const processedPackageJson: PackageJson = {
+			...rest
+		};
+
+		processedPackageJson.exports = {
+			".": {
+				import: "./index.js",
+				types: "./index.d.ts"
+			}
+		};
+
+		// Write processed package.json to dist
+		await writeFile(join(process.cwd(), "dist", "package.json"), JSON.stringify(processedPackageJson, null, "\t"));
 	}
 });
