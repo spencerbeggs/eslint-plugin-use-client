51dc1cc22c4f0d0945071784a9ccc3bddf4db62b
C. Spencer Beggs <spencer@beg.gs>
2025-04-21 16:17:56 -0400

    refactor(tests): remove obsolete fixture components
    deletes various test fixture components that are no longer in use to streamline the testing process.
    
    This cleanup reduces clutter and improves maintainability by removing unnecessary code, which can help prevent confusion in the testing suite.

--- pkg/__test__/__fixtures__/missing-directive.tsx
+++
@@ -1,6 +0,0 @@
-import { useState } from "react";
-
-export default function MissingDirective() {
-	const [state] = useState(0);
-	return <div>{state}</div>;
-}
--- pkg/__test__/__mocks__/next.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/require-await
-export const headers = async () => ({ get: (_key: string) => "mock-user-agent" });
-export const useRouter = () => ({
-	pathname: "/",
-	push: (_pathname: string) => {
-		return true;
-	}
-});
-export const usePathname = () => "/";
